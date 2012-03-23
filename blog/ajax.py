a35ecc425ca1ee5704922f0e0c6bec0a202588c3
RegisF <regis.floret@gmail.com>
2012-03-23 11:40:34 +0400

    name collision for ajax calls

--- blog/ajax.py
+++ blog/ajax.py
@@ -29,23 +29,21 @@ from django.http import HttpResponse, HttpResponseBadRequest
 from django.core import serializers
 
 from blog.models import Post, Preference
+from notification import ajax_log
 
 def getnext(request):
-    #time.sleep(2)
     try:
-        if request.is_ajax():
-            articles = int(request.GET.get("articles"))
-            pref = Preference.objects.get(id=1)
-            ajax_log("Count %d " % articles)
-            context = RequestContext(request,{
-                'blog' : {
-                    'get_last_posts' : Post.objects.filter(Publish=True)[articles:articles+5],
-                    'showFullArticle' : pref.ShowFullArticle,
-                    'articleSampleLength' : pref.ArticleSampleLength
-                }
-            })
+        articles = int(request.GET.get("articles"))
+        pref = Preference.objects.get(id=1)
+        context = RequestContext(request,{
+            'blog_last' : {
+                'get_last_posts' : Post.objects.filter(Publish=True)[articles:articles+5],
+                'showFullArticle' : pref.ShowFullArticle,
+                'articleSampleLength' : pref.ArticleSampleLength
+            }
+        })
         return HttpResponse(loader.get_template('blog/nextpost.html').render(context))    
     except Exception as e:
-        ajax_log("GetNext : %s" % e)
-    
+        ajax_log("%s" % e)
+
     return HttpResponseBadRequest("Bad request")
