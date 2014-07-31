5e9ce8d4d9b25d02215a0f0ae3e0d6317f754880
Regis Floret <regis.floret@gmail.com>
2014-07-31 19:48:09 +0400

    Add new elements

---
+++ models/README.md
@@ -0,0 +1,4 @@
+YaBLOG 2 Models
+===============
+
+This directory contains all models for the blog
---
+++ views/home.py
@@ -0,0 +1,38 @@
+# -*- coding: UTF-8 -*-
+# YaBlog - Yet Another BLOG engine
+#  (c) Regis FLORET 2011 and mater
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#    * Neither the name of the <organization> nor the
+#      names of its contributors may be used to endorse or promote products
+#      derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL Regis FLORET BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+__author__ = 'Régis FLORET'
+
+from django.views.generic import ListView
+from django.conf import settings
+
+from yablog.models.post import Post
+
+class HomeView(ListView):
+    template_name = "{}{}".format(settings.ACTIVE_THEME, "index.html")
+    model = Post
+    paginate_by = settings.MAIN_PAGE_POST_COUNT
+    context_object_name = 'posts'
